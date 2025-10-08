2f2118885f2d10c1d1057c526c4d4aa9c7b545b1
mrfortuna11 <dima12513@gmail.com>
2025-10-08 08:17:46 +0300

    local_shadertoy2

---
+++ tasks/local_shadertoy2/shaders/UniformParams.h
@@ -0,0 +1,15 @@
+#ifndef UNIFORM_PARAMS_H_INCLUDED
+#define UNIFORM_PARAMS_H_INCLUDED
+
+#include "cpp_glsl_compat.h"
+
+
+struct UniformParams
+{
+  shader_vec2  iResolution;
+  shader_vec2  iMouse;
+  shader_float iTime;
+};
+
+
+#endif // UNIFORM_PARAMS_H_INCLUDED
