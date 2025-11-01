2d35f9f903db71a1199a9c13636558675d60c1a3
mrfortuna11 <dima12513@gmail.com>
2025-11-01 17:55:11 +0300

    task 2 done

--- tasks/model_bakery/renderer/WorldRenderer.cpp
+++ tasks/model_bakery/renderer/WorldRenderer.cpp
@@ -28,7 +28,7 @@ void WorldRenderer::allocateResources(glm::uvec2 swapchain_resolution)
 
 void WorldRenderer::loadScene(std::filesystem::path path)
 {
-  sceneMgr->selectScene(path);
+  sceneMgr->selectScene(path, SceneManager::SceneAssetType::BAKED);
 }
 
 void WorldRenderer::loadShaders()
